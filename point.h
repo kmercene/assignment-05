cf67a3fc082be0dca0f36cd7421af80d0cd4d100
kmercene <kmercene@csu.fullerton.edu>
2016-05-01 20:14:41 -0700

    Create point.h

---
+++ point.h
@@ -0,0 +1,14 @@
+#ifndef POINT_H
+#define POINT_H
+
+class Point{
+	private:
+		float x;
+		float y;
+	public:
+		Point(float x=0, float y=0);
+		float getx();
+		float gety();
+};
+
+#endif  // POINT_H
